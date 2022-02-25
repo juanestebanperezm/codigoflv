df83540630a04dbe1e85750d5262833a5755a0ac
juanestebanperezm <flievano100@misorobotics.com>
2022-02-25 12:00:31 -0500

    siiiiuu

--- frontend/src/components/Preparate.js
+++ frontend/src/components/Preparate.js
@@ -35,16 +35,6 @@ function Preparate() {
 
          </div>
        } )}
+      </div>)}
        
-import React from 'react'
-import "../styles/Preparate.css"
-
-function Preparate() {
-  return (
-    <div>
-        <h1>Preparate works!</h1>
-    </div>
-  )
-}
-
 export default Preparate

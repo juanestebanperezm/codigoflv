0576ac90930bc8fd144fbd7aa0ac001b4b26a501
juanestebanperezm <flievano100@misorobotics.com>
2022-02-26 14:54:28 -0500

    registro

--- frontend/src/components/Registro.js
+++ frontend/src/components/Registro.js
@@ -1,16 +1,41 @@
-import React from 'react'
+import React from "react";
 
-import styles from  "../styles/Registro.module.css"
+import styles from "../styles/Registro.module.css";
 
 function Registro() {
   return (
-    <body>
-
-    <div className={styles["container"]}>
+    
+      <div className={styles["input-fields"]}>
+        <label className={styles["chapa"]}>Nickname, apodo, chapa o como queras llamarte</label>
+        <input
+          type="text"
+          placeholder="Nombre de usuario"
+          className={styles["inlinein"]}
+        ></input>
+        <label className={styles["chapa"]}>Correo</label>
+        <input
+          type="email"
+          placeholder="Email"
+          className={styles["inlinein"]}
+        ></input>
+        <label className={styles["chapa"]}>La clave y ojo pone algo bien facil</label>
+        <input
+          type="password"
+          placeholder="Contraseña"
+          className={styles["inlinein"]}
+        ></input>
+        <label className={styles["chapa"]}>Repita la clave</label>
+        <input
+          type="password"
+          placeholder="Confirma tu contraseña"
+          className={styles["inlinein"]}
+        ></input>
         
-    </div>
-</body>
-  )
+        <button className="name noselect">Registrate</button>
+      </div>
+     
+    
+  );
 }
 
-export default Registro
+export default Registro;

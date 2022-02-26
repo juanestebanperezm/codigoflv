1789188967cd633f46211ad1a35bf515a63cba53
Mauricio Barva <mauriciobarva@gmail.com>
2022-02-25 20:34:04 -0500

    metodo toJSON sobreescrito

--- backend/modelos/Retos.mjs
+++ backend/modelos/Retos.mjs
@@ -5,12 +5,6 @@ const {Schema,model}=moongose;
 //Este esquema require poder almacenar un array donde se establezcan las categorias de un reto
 //Ya que puede tener varias, Matematicas, estructuras de datos, string y muchas de estas combinadas :O 
 
-const options = {
-    toJSON: { virtuals: true },
-    toObject: { virtuals: true },
-}
-
-
 const challengeSchema = new Schema({
     title: {
         type: String,
@@ -21,15 +15,22 @@ const challengeSchema = new Schema({
         required: [true, "Aqui tenes que explicar la gente no adivina lo que estas tratando de inventar"]
     },
     language: {
-        type: String,
+        type: [String],
         required: false
     },
     dificultad: {
         type: String,
         required: [false]
     }
-}, options);
+});
 
-const Reto=model("challenge",challengeSchema);
+
+challengeSchema.methods.toJSON = function () {
+    const {__v, _id, ...challenges} = this.toObject();
+    challenges.id = _id;
+    return challenges;
+}
+
+const Reto = model("challenge", challengeSchema);
  
-export default Reto
+export default Reto;

a795962149a879ae998df2eadd0952c0a9e88afe
Mauricio Barva <mauriciobarva@gmail.com>
2022-02-25 20:50:02 -0500

    obtener usuario por id y su respectivo validador añadido

--- backend/routes/challenge.mjs
+++ backend/routes/challenge.mjs
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import { param } from 'express-validator';
 
 //Controladores
 import { addChallenge, viewOneChallenge ,getChallenges ,deleteChallenge } from "../controllers/challenge.mjs";
+import { validateChallengeID } from "../database/challenge-validators.mjs";
+import { validateErrors } from "../middlewares/validate-errors.mjs";
 
 /*Router de los retos*/
 const router = Router();
@@ -10,12 +13,19 @@ const router = Router();
 router.get('/', getChallenges);
 
 /*Obtener reto por id*/
-router.get('/:id', viewOneChallenge);
+router.get('/:id', [
+    param('id','El id del reto a obtener es obligatorio y debe ser un id de mongo.').isMongoId().custom(validateChallengeID),
+    validateErrors
+] , viewOneChallenge);
+
 
 /*Añadir un reto*/
 router.post('/', addChallenge);
 
 /*Borrar un reto por id*/
-router.delete('/:id', deleteChallenge);
+router.delete('/:id', [
+    param('id', 'El id del reto a borrar es obligatorio y debe ser un id de mongo.').isMongoId().custom(validateChallengeID),
+    validateErrors
+], deleteChallenge);
 
 export default router;

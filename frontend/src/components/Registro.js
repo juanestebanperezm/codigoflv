91d3ba35915cdc3de6b85b91402a0272b0700447
juanestebanperezm <flievano100@misorobotics.com>
2022-02-26 23:28:38 -0500

    post de usuarios desde react a express

--- frontend/src/components/Registro.js
+++ frontend/src/components/Registro.js
@@ -1,75 +1,72 @@
-import React, { useState } from "react";
-/* import axios, { AxiosResponse, AxiosInstance } from 'axios'; */
+import React, { useEffect, useState } from "react";
 import styles from "../styles/Registro.module.css";
 
 function Registro() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const URL="http://localhost:3000/usuarios"
+  const URL = "http://localhost:3000/usuarios";
 
-  const [data,setData]=useState({
-    name:"",
-    email:"",
-    password:""
-  });
-
-  function submit(e){
+  const handleSubmit = (e) => {
     e.preventDefault();
-    /* axios.post(URL,{
-      name:data.name,
-      email:data.email,
-      password:data.password
-    })
-    .then(res=>{
-      console.log(res.data)
-    }) */
-  }
+    const user = { name, email, password };
 
-  function handle(e){
-    const newData={...data}
-    newData[e.target.id]=e.target.value
-    setData(newData)
-    console.log(newData)
-  }
+    fetch(URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(user),
+    }).then(() => {
+      console.log(":)");
+    });
+  };
 
   return (
-    
-     <div className={styles["fondo"]}>
-       <form onSubmit={(e)=>submit(e)}>
-       <div className={styles["input-fields"]}>
-        <label className={styles["chapa"]}>Nickname, apodo, chapa o como queras llamarte</label>
-        <input
-          type="text"
-          placeholder="Nombre de usuario"
-          className={styles["inlinein"]}
-          name="name"
-        ></input>
-        <label className={styles["chapa"]}>Correo</label>
-        <input
-          type="email"
-          placeholder="Email"
-          className={styles["inlinein"]}
-          name="email"
-        ></input>
-        <label className={styles["chapa"]}>La clave y ojo pone algo bien facil</label>
-        <input
-          type="password"
-          placeholder="Contraseña"
-          className={styles["inlinein"]}
-          name="password"
-        ></input>
-      {/*   <label className={styles["chapa"]}>Repita la clave</label>
+    <div className={styles["fondo"]}>
+      <form onSubmit={handleSubmit}>
+        <div className={styles["input-fields"]}>
+          <label className={styles["chapa"]}>
+            Nickname, apodo, chapa o como queras llamarte
+          </label>
+          <input
+            type="text"
+            placeholder="Nombre de usuario"
+            className={styles["inlinein"]}
+            name="name"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          ></input>
+          <label className={styles["chapa"]}>Correo</label>
+          <input
+            type="email"
+            placeholder="Email"
+            className={styles["inlinein"]}
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          ></input>
+          <label className={styles["chapa"]}>
+            La clave y ojo pone algo bien facil
+          </label>
+          <input
+            type="password"
+            placeholder="Contraseña"
+            className={styles["inlinein"]}
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          ></input>
+          {/*   <label className={styles["chapa"]}>Repita la clave</label>
         <input
           type="password"
           placeholder="Confirma tu contraseña"
           className={styles["inlinein"]}
         ></input> */}
-        
-        <button className={styles["name noselect"]}>Registrate</button>
-      </div>
-       </form>
-     </div>
-     
-    
+
+          <button className={styles["name noselect"]}>Registrate</button>
+        </div>
+      </form>
+    </div>
   );
 }
 

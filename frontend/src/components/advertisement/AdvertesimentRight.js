6c8fd2c6819246fa928deb595261ad85c93ee389
ema4hhh <ema4hhh@gmail.com>
2022-02-28 18:46:59 -0500

    añadido los anuncios laterales

---
+++ frontend/src/components/advertisement/AdvertesimentRight.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import monaChina from '../../styles/assets/temporalAdvertisement2.jpg'
+import '../../styles/Advertesiment.css'
+
+function AdvertesimentRight() {
+  return (
+    <div class="sidenavRight">
+      <img src={ monaChina } alt="Anuncio pa' comer" />
+      <a>espero no que esté cuando pongan contenido</a>
+    </div>
+  )
+}
+
+export default AdvertesimentRight;

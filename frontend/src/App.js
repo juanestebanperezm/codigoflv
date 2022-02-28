2aeed7a5bb58a20288630db60c67b7fcfdce2714
ema4hhh <ema4hhh@gmail.com>
2022-02-28 18:51:18 -0500

    añadido estilos, imágenes temporales y  <Advertisement /> a App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 //Componentes
 import Cabecera from "./components/Cabecera";
 import Footer from "./components/Footer";
+import Advertisement from "./components/advertisement/Advertisement";
 
 function App() {
  
   return (
     <div className="App">
       <Cabecera />
+      <Advertisement />
       <Footer/>
     </div>
   );

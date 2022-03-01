cbb6e376012a2d1e141d10f581a206b522bbca38
Diego-Figue <diegofigueroa864@gmail.com>
2022-02-28 22:42:37 -0500

    sidebar x2

---
+++ frontend/src/components/Lateral.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "../styles/js styles/efectos.js";
+import "../styles/Lateral.css";
+
+function Lateral() {
+  return (
+    <div className="contenedor">
+      <div className="btnCerrar">
+        <span className="arrowR"></span>
+      </div>
+      <div className="btnAbrir">
+        <span className="arrowL"></span>
+      </div>
+      <a href="#" className="btn">
+        Unete A la Familia
+      </a>
+      <span className="line"></span>
+      <div>
+        <a href="#" className="item">
+          Python
+        </a>
+        <a href="#" className="item">
+          JavaScript
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Lateral;

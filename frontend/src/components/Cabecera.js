5242637136ea034447f496c5da9137adf2906fff
ema4hhh <ema4hhh@gmail.com>
2022-03-01 19:55:06 -0500

    browserRouting

--- frontend/src/components/Cabecera.js
+++ frontend/src/components/Cabecera.js
@@ -1,18 +1,9 @@
-import { BrowserRouter as Router, Switch, Route, Link, Routes, BrowserRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
 import React from "react";
 
-// Componentes Externos
+//ROUTER
 
-import Certificado from "./Certificado"
-import Cuenta from "./Cuenta"
-import Compite from "./Compite"
-
-
-import Login from "./Login";
-import Reto from "./Reto"
-import Preparate from "../components/preparate/Preparate";
-import Registro from "./Registro";
-import RegistroEmpresas from "./RegistroEmpresas";
+import ROUTES, { RenderRoutes } from "../routes";
 
 
 //ESTILOS
@@ -21,56 +12,43 @@ import styles from "../styles/Cabecera.module.css";
 
 function Cabecera() {
   return (
-    <BrowserRouter>
-      <div >
-        <div className={styles.header}>
-          <nav>
-            <div className={styles.container}>
-              <ul className={styles['nav-links']}>
-                <li className={styles.logo}>
-                  <a>Logo</a>
-                </li>
-                <li className={styles['nav-link-item']}>
-                  <Link to="/preparate">Preparate</Link>
-                </li>
-                <li className={styles['nav-link-item']}>
-                  <Link to="/certificate">Certificado</Link>
-                </li>
-                <li className={styles['nav-link-item']}>
-                  <Link to="/compite">Compite</Link>
-                </li>
-              </ul>
-              <div className={styles.container}>
-                <ul className={styles['nav-links']}>
-                  <li className={styles['nav-link-item']}>
-                    <Link to="/registro">Registro developer</Link>
-                  </li>
-                  <li className={styles['nav-link-item']}>
-                    <Link to="/registroempresa">Para Empresas</Link>
-                  </li>
-                  <li className={styles['nav-link-item']}>
-                    <Link to="/cuenta">Cuenta</Link>
-                  </li>
-                  <li className={styles['nav-link-item']}>
-                    <Link to="/login">Login</Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
+    <div className={styles.header}>
+      <nav>
+        <div className={styles.container}>
+          <ul className={styles['nav-links']}>
+            <li className={styles.logo}>
+              Logo
+            </li>
+            <li className={styles['nav-link-item']}>
+              <Link to="/preparate">Preparate</Link>
+            </li>
+            <li className={styles['nav-link-item']}>
+              <Link to="/certificate">Certificado</Link>
+            </li>
+            <li className={styles['nav-link-item']}>
+              <Link to="/compite">Compite</Link>
+            </li>
+          </ul>
+          <div className={styles.container}>
+            <ul className={styles['nav-links']}>
+              <li className={styles['nav-link-item']}>
+                <Link to="/registro">Registro developer</Link>
+              </li>
+              <li className={styles['nav-link-item']}>
+                <Link to="/registroempresa">Para Empresas</Link>
+              </li>
+              <li className={styles['nav-link-item']}>
+                <Link to="/cuenta">Cuenta</Link>
+              </li>
+              <li className={styles['nav-link-item']}>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          </div>
         </div>
-        <Routes>
-          <Route exact path="/preparate" element={<Preparate/>}/>
-          <Route exact path="/certificate" element={<Certificado/>}/>
-          <Route exact path="/compite" element={<Compite/>}/>
-          <Route exact path="/reto/:id" element={<Reto/>}/>
-          <Route exact path="/cuenta" element={<Cuenta/>}/>
-          <Route exact path="/login" element={<Login/>}/>
-          <Route exact path="/registro" element={<Registro/>}/>
-          <Route exact path="/registroempresa" element={<RegistroEmpresas/>}/>
-        </Routes>
-      </div>
-    </BrowserRouter>
+      </nav>
+      <RenderRoutes routes={ROUTES} />
+    </div>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 
 //Bustrap
 /* import 'bootstrap/dist/css/bootstrap.css'; */
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 

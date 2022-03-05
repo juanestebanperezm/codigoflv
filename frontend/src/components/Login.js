39f4bab785975c4f7106b2aebfc841ef5593025c
ema4hhh <ema4hhh@gmail.com>
2022-03-05 14:30:09 -0500

    implemetado Fork al formulario

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,95 +1,39 @@
-import {
-  Box,
-  FormControl,
-  IconButton,
-  InputAdornment,
-  InputLabel,
-  OutlinedInput,
-} from "@material-ui/core";
-import { makeStyles } from "@material-ui/styles";
-import { Button } from "@mui/material";
-import { Link } from "react-router-dom";
-import Visibility from "@material-ui/icons/Visibility";
-import VisibilityOff from "@material-ui/icons/VisibilityOff";
-import marcaAgua from "../assets/codigo-sancocho.png";
 import React, { useState } from "react";
 
-import "../styles/Login.css";
+import * as Yup from "yup";
+import { Box } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import { Button, TextField } from "@mui/material";
+import { Link } from "react-router-dom";
+import { Field, Form, Formik, useFormik } from "formik";
 
-const useStyles = makeStyles({
-  container: {
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: "column",
-    alignItems: "center",
-    width: "100%",
-    height: "100vh",
-    margin: "auto",
-  },
-  form: {
-    width: "75%",
-    height: "70vh",
-    padding: "10px",
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    borderRadius: "0 0 5px 5px",
-    flexDirection: "column",
-    backgroundColor: "rgb(39, 38, 64)",
-    boxShadow: "1px 5px 6px 1px rgb(0 0 0 / 20%)",
-    "@media (max-width:600px)": {
-      width: "92%",
-    },
-  },
-  textField: {
-    marginBottom: "10px",
-    width: "100%",
-    color: "#2EFF22",
-  },
-  linkRegister: {
-    color: "black",
-    marginLeft: "3px",
-    "&:hover": {
-      color: "#212f45",
-    },
-  },
-  image: {
-    width: "100%",
-  },
-  containerForm: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between",
-    padding: "20px",
-    height: "100%",
-  },
-  titleSection: {
-    display: "flex",
-    alignItems: "center",
-    flexDirection: "column",
-    justifyContent: "center",
-    width: "38%",
-    height: "55vh",
-    borderRight: "1px solid rgba(0,0,0,0.3)",
-  },
-  fieldsForm: {
-    width: "58%",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column",
-  },
-  sectionLink: {
-    marginTop: "10px",
-  },
-  label: {
-    color: "white",
-  },
+import marcaAgua from "../assets/codigo-sancocho.png";
+
+import useStyles from "./Login.styles";
+
+const LoginValidation = Yup.object({
+  email: Yup
+    .string('Enter your email')
+    .email('Enter a valid email')
+    .required('Email is required'),
+  password: Yup
+    .string('Enter your password')
+    .min(8, 'Password should be of minimum 8 characters length')
+    .required('Password is required'),
 });
 
 function Login({ openLogin, setOpenLogin }) {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: LoginValidation,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   
-
   const classes = useStyles();
 
   const [password, setPassword] = useState({
@@ -116,42 +60,38 @@ function Login({ openLogin, setOpenLogin }) {
             />
           </Box>
           <Box className={classes.fieldsForm}>
-            <FormControl variant="outlined" className={classes.textField}>
-              <InputLabel className={classes.label}>Usuario</InputLabel>
-              <OutlinedInput type="text" labelWidth={90} />
-            </FormControl>
-            <FormControl variant="outlined" className={classes.textField}>
-              <InputLabel className={classes.label}>Contraseña</InputLabel>
-              <OutlinedInput
-                type={password.showPassword ? "text" : "password"}
-                value={password.password}
-                onChange={handleChangePassword}
-                labelWidth={90}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton onClick={handleClickShowPassword} edge="end">
-                      {password.showPassword ? (
-                        <Visibility />
-                      ) : (
-                        <VisibilityOff />
-                      )}
-                    </IconButton>
-                  </InputAdornment>
-                }
-              />
-            </FormControl>
-            <Button
-              color="primary"
-              variant="contained"
+          <form onSubmit={formik.handleSubmit}>
+            <TextField
+              fullWidth
+              id="email"
+              name="email"
+              label="Email"
               sx={{
-                backgroundColor: "#0b525b !important",
-                display: "block",
-                width: "100%",
-                marginTop: "10px",
+                marginBottom: "15px"
               }}
-            >
-              Login
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email}
+            />
+            <TextField
+              fullWidth
+              id="password"
+              name="password"
+              label="Password"
+              type="password"
+              sx={{
+                marginBottom: "15px"
+              }}
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              error={formik.touched.password && Boolean(formik.errors.password)}
+              helperText={formik.touched.password && formik.errors.password}
+            />
+            <Button color="primary" variant="contained" fullWidth type="submit">
+              Submit
             </Button>
+          </form>
             <Box className={classes.sectionLink}>
               <Box component="p">
                 ¿Aún no tiene cuenta parce?

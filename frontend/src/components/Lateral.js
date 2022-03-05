82b5a41327a49ac1f1184ee0bfdf686fc3a89e8b
juanestebanperezm <flievano100@misorobotics.com>
2022-03-05 16:55:23 -0500

    lateral corregida

--- frontend/src/components/Lateral.js
+++ frontend/src/components/Lateral.js
@@ -4,25 +4,8 @@ import  "../styles/Lateral.css";
 
 function Lateral() {
   return (
-    <div className="contenedor">
-    <div className="btnCerrar">
-      <span className="arrowR"></span>
-    </div>
-    <div className="btnAbrir">
-      <span className="arrowL"></span>
-    </div>
-    <a href="#" className="btn">
-      Unete A la Familia
-    </a>
-    <span className="line"></span>
-    <div>
-      <a href="#" className="item">
-        Python
-      </a>
-      <a href="#" className="item">
-        JavaScript
-      </a>
-    </div>
+    <div >
+    
   </div>
   );
 }
